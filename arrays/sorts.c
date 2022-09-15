1e3a1f53ed2b585998022230e784176b121389a9
onlymachiavelli <tmakaveli643@gmail.com>
2022-09-15 09:22:47 +0100

    made a mess

--- arrays/sorts.c
+++ arrays/sorts.c
@@ -1,10 +1,36 @@
 #include <stdio.h> ;
-bubSort(int &arr[5])
+
+#include <stdbool.h>
+
+void printFunc(int numbers[5])
 {
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d \n", numbers[i]);
+    }
+}
+void bubSort(int *numbers)
+{
+
+    bool check = false;
+    while (!check)
+    {
+        check = true;
+        for (int i = 0; i < 4; i++)
+        {
+            if (numbers[i] > numbers[i + 1])
+            {
+                numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i];
+                check = false;
+            }
+        }
+    }
 }
 int main()
 {
 
     int arr[5] = {4, 66, 3254, 879, 3};
+    bubSort(&arr);
+    printFunc(arr);
     return 0;
 }
