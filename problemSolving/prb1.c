8c1332b855adacb5761864e54954dc0a35c14bae
onlymachiavelli <tmakaveli643@gmail.com>
2022-09-17 20:47:05 +0100

    solved

--- problemSolving/prb1.c
+++ problemSolving/prb1.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
 
-#define
+#define Main int main
+Main()
+{
+
+    int t, x, y, result;
+
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        scanf("%d", &x);
+        scanf("%d", &y);
+        result = y > x ? y - x : 0;
+        printf("%d \n", result);
+    }
+    return 0;
+}
