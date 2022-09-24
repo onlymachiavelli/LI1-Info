085c36a92f8e87de44a25dcc2ccdcae4de9fa441
onlymachiavelli <tmakaveli643@gmail.com>
2022-09-24 15:58:53 +0100

    anal serie not

--- arrays/sorts.c
+++ arrays/sorts.c
@@ -1,36 +1,11 @@
-#include <stdio.h> ;
+#include <stdio.h>
 
-#include <stdbool.h>
+// sous programme
 
-void printFunc(int numbers[5])
-{
-    for (int i = 0; i < 5; i++)
-    {
-        printf("%d \n", numbers[i]);
-    }
-}
-void bubSort(int *numbers)
+// fonction main (programme principal ! )
+void main()
 {
 
-    bool check = false;
-    while (!check)
-    {
-        check = true;
-        for (int i = 0; i < 4; i++)
-        {
-            if (numbers[i] > numbers[i + 1])
-            {
-                numbers[i], numbers[i + 1] = numbers[i + 1], numbers[i];
-                check = false;
-            }
-        }
-    }
-}
-int main()
-{
-
-    int arr[5] = {4, 66, 3254, 879, 3};
-    bubSort(&arr);
-    printFunc(arr);
-    return 0;
+    int age = 20;
+    printf("%d \n", age);
 }
