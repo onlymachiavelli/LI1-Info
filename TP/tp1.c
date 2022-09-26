10238502462fd16f11e890d009d6d9b28e3bb298
onlymachiavelli <tmakaveli643@gmail.com>
2022-09-26 09:59:45 +0100

    tp1 shitty

--- TP/tp1.c
+++ TP/tp1.c
@@ -5,7 +5,24 @@
 Main()
 {
 
-    printf("Test empty code ! \a");
+    int n = 4, m = 9;
+    printf("The sum of %d and %d is :%d \n", n, m, n + m);
+    printf("The Prod of %d and %d is :%d \n", n, m, n * m);
+
+    // with real type vars
+
+    float v = 4.20, w = 6.9;
+    printf("The sum of %2.6f and %2.6f is :%2.6f \n", v, w, v + w);
+    printf("The Prod of %2.6f and %2.6f is :%2.6f \n", v, w, v * w);
+
+    // second ex
+    int x = 69, y = 420;
+
+    printf("The x is %d and the y is %d \n", x, y);
+    int swapp = x;
+    x = y;
+    y = x;
+    printf("Now after the swapp , The x is %d and the y is %d \n ", x, y);
 
     return 0;
 }
