8686f98e09dac3ab1c934a5f8e15245d62b3a6d4
onlymachiavelli <tmakaveli643@gmail.com>
2022-10-07 13:57:11 +0100

    finished TP2  !

--- TP/TP2/tp2.c
+++ TP/TP2/tp2.c
@@ -2,19 +2,14 @@
 
 void main()
 {
-    int a = 20, b = 5, c = -10, d = 2, x = 12, y = 15;
-    printf("%d", (5 * x) + 2 * ((3 * b) + 4));
-
-    printf("%d \n", (5 * (x + 2) * 3) * (b + 4));
-    printf("%i \n", a == (b = 5));
-    printf("%d \n", a += (x + 5));
-    printf("%d \n", a != (c *= (-d)));
-    printf("%d \n", a %= d++);
-    printf("%d \n", a %= ++d);
-    printf("%d \n", (x++) * (a + c));
-    printf("%d \n", a = x * (b < c) + y * !(b < c));
-    printf("%d \n", !(x - d + c) || d);
-    printf("%d \n", a && b || !0 && c && !d);
-    printf("%d \n", ((a && b) || (!0 && c)) && !d);
-    printf("%d \n", ((a && b) || !0) && (c && (!d)));
+    int x, y, swapp;
+    printf("Enter X ! \n");
+    scanf("%d", &x);
+    printf("Enter Y! \n");
+    scanf("%d", &y);
+    printf("X = %d and Y = %d \n", x, y);
+    swapp = x;
+    x = y;
+    y = swapp;
+    printf("X = %d and Y = %d", x, y);
 }
