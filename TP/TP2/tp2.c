c1deb68a84dc2866dab634d0f799033ea4ee3aef
onlymachiavelli <tmakaveli643@gmail.com>
2022-10-10 09:56:21 +0100

    exercice

--- TP/TP2/tp2.c
+++ TP/TP2/tp2.c
@@ -23,7 +23,7 @@ void ex_3_3()
 {
 
     int a = 20, b = 5, c = -10, d = 2, x = 12, y = 15;
-    printf("%d", (5 * x) + 2 * ((3 * b) + 4));
+    printf("%d \n", (5 * x) + 2 * ((3 * b) + 4));
 
     printf("%d \n", (5 * (x + 2) * 3) * (b + 4));
     printf("%i \n", a == (b = 5));
@@ -32,8 +32,9 @@ void ex_3_3()
     printf("%d \n", a %= d++);
     printf("%d \n", a %= ++d);
     printf("%d \n", (x++) * (a + c));
+
     printf("%d \n", a = x * (b < c) + y * !(b < c));
-    printf("%d \n", !(x - d + c) || d);
+    printf("%d the fuck !  \n", !(x - d + c) || d);
     printf("%d \n", a && b || !0 && c && !d);
     printf("%d \n", ((a && b) || (!0 && c)) && !d);
     printf("%d \n", ((a && b) || !0) && (c && (!d)));
