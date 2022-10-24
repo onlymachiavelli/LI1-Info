1513c9b667d2b6482fa93ba5274bd681ede6a4ea
onlymachiavelli <tmakaveli643@gmail.com>
2022-10-24 09:07:22 +0100

    fixing ex5

--- TP/TP3/tp3_ex5.c
+++ TP/TP3/tp3_ex5.c
@@ -2,15 +2,15 @@
 int main()
 {
 
-    float r1, r2, r3;
+    double r1, r2, r3;
     char plug;
     printf("Enter R1, R2 and R3 \n");
-    scanf("%f %f %f", &r1, &r2, &r3);
+    scanf("%lf %lf %lf", &r1, &r2, &r3);
     printf("Enter the PLug type ! \n p-parallel\n other-serie\n");
     scanf("%s", &plug);
     if (plug == 'p')
     {
-        printf("The result is %f", 1 / ((1 / r1) + (1 / r2) + (1 / r3)));
+        printf("The result is %f", (r1 * r2 * r3) / (r1 * (r2 + r3) + r1 * r3));
     }
     else
     {
