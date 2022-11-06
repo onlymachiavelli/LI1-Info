1dcb000e6fb35120919dddbdcf7ec83d3bb35add
onlymachiavelli <tmakaveli643@gmail.com>
2022-11-06 22:56:27 +0100

    from scratsh

--- WorkingWithTheBoys/frass1.py
+++ WorkingWithTheBoys/frass1.py
@@ -2,33 +2,28 @@ import numpy as np
 
 
 def size(min: int, max: int) -> int:
-    n = input(int("Enter number of computers "))
+    n = int(input())
     if min <= n <= max:
         return n
-    return size(n, min, max)
+    return size(min, max)
 
 
-def fillCodes(n: int, arr):
+def fillArr(n, codes, model, price):
     for i in range(n):
-        print("enter the code ! ")
-        arr[i] = size(1000, 9999)
+        print("Enter Code ! ")
+        print("Enter the code ")
+        codes[i] = size(1000, 9999)
 
-
-def fillModel(n: int, arr):
-    for i in range(n):
-        print("Enter the model ! ")
+        print("Enter model ! ")
         while True:
-            arr[i] = input()
-            if 65 <= ord(arr[i][0]) <= 90:
+            model[i] = input()
+            if 65 <= ord(model[i][0]) <= 90:
                 break
 
-
-def fillPrice(n: int, arr):
-    for i in range(n):
-        print("Enter the price")
+        print("Enter the price ")
         while True:
-            arr[i] = float(input())
-            if arr[i] > 0:
+            price[i] = float(input())
+            if price[i] > 0:
                 break
 
 
@@ -43,12 +38,46 @@ def triZawali(n: int, arr, err, gadem):
                 gadem[i], gadem[i+1] = gadem[i+1], gadem[i]
 
 
-def expensive(n: int, arr, err, gadem):
-    res = np.array(n, {})
-    m = 0
-
+def exist(n, arr, model: str) -> bool:
     for i in range(n):
-        name = arra[]
+        print(arr)
+        if arr[i]["model"] == model:
+            return True
+    return False
 
 
-code = np.array()
+def expensive(n: int, arr, err, gadem):
+    res = np.zeros(n, dtype=object)
+    m = 0
+    for i in range(n):
+        select = {
+            "code": arr[i],
+            "model": err[i],
+            "price": gadem[i]
+        }
+        if not exist(m, res, select["model"]):
+            for j in range(n):
+                if gadem[j] > select["price"]:
+                    select = {
+                        "code": arr[j],
+                        "model": err[j],
+                        "price": gadem[j]
+                    }
+            res[m] = select
+            m += 1
+
+    for i in range(m):
+        print("the most expensive item in ",
+              m[i]["model"], " is worth : ", m[i]["price"])
+
+
+print("Enter the number of computers ")
+n = size(3, 10)
+
+code = np.zeros(n, dtype=int)
+price = np.zeros(n, dtype=float)
+model = np.zeros(n, dtype=str)
+
+fillArr(n, code, model, price)
+triZawali(n, code, model, price)
+expensive(n, code, model, price)
