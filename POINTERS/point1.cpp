76f51fff046b1ed08d02a79fc7caf938ae9281b6
onlymachiavelli <tmakaveli643@gmail.com>
2022-11-09 00:20:59 +0100

    topg list

--- POINTERS/point1.cpp
+++ POINTERS/point1.cpp
@@ -2,11 +2,29 @@
 
 using namespace std; 
 
-int main () {
-    int n = 5;  
-    cout << n << endl << &n ;
-    int *adress = &n; 
-    cout << endl<<adress; 
 
+struct Node{ 
+
+
+    int head; 
+    struct Node*next ; 
+} ; 
+int main () {
+    struct Node*TopG ; 
+    struct Node*data; 
+
+    TopG = (struct Node *) malloc(sizeof(struct Node)) ; 
+    data = (struct Node*) malloc(sizeof(struct Node)) ; 
+    TopG->head = 1; 
+    TopG->next->head=2; 
+    TopG->next->next->head = 3 ; 
+
+    data->head = TopG->head; 
+    while (data) {
+        printf("%dhello world" , data->head) ; 
+        data->head = TopG->next->head;
+
+
+    }
     return 0 ; 
-}
+} 
