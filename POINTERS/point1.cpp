b526b41d0212e3d29751868eeba2d37a50be7ee0
onlymachiavelli <tmakaveli643@gmail.com>
2022-11-08 21:50:34 +0100

    taking a look at pointers

---
+++ POINTERS/point1.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std; 
+
+int main () {
+    int n = 5;  
+    cout << n << endl << &n ;
+    int *adress = &n; 
+    cout << endl<<adress; 
+
+    return 0 ; 
+}
