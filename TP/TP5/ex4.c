db1c74aefd6037f541f41016c90c8c95871bc9fd
onlymachiavelli <tmakaveli643@gmail.com>
2022-11-14 12:36:50 +0100

    ex 4

--- TP/TP5/ex4.c
+++ TP/TP5/ex4.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
+
+void printArr(int n, int arr[n])
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ,", arr[i]);
+    }
+}
 int main()
 {
+
+    int n;
+    printf("enter the length \n");
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        printf("enter value of index %d\n", i);
+        scanf("%d", &arr[i]);
+    }
+
+    int max = 0, min = arr[0], iMin = 0, iMax, check = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+            iMax = i;
+        }
+        else if (arr[i] < min)
+        {
+            min = arr[i];
+            iMin = i;
+        }
+    }
+
+    printf("The array is : \n");
+    printArr(n, arr);
+    printf("\n");
+    printf("the min is %d with index %d and the max is %d with index %d", min, iMin, max, iMax);
 
     return 0;
 }
