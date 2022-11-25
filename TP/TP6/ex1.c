a2e5d7de2015b739e4d80fbf6bf6723ebe3a7da8
onlymachiavelli <tmakaveli643@gmail.com>
2022-11-25 13:26:23 +0100

    cleaning abnd ex 1

--- TP/TP6/ex1.c
+++ TP/TP6/ex1.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-
 int main()
 {
 
-    char *days[8] = {
-        "The're no day with that number ! ", "Monday", "Tuesday", "Wednesday", "Thursday ", "Friday", "Saturday", "Sunday"};
+    char *days[8] =
+        {
+            "Day Doesn't exist",
+            "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
     int day;
-    printf("Enter the day number ! \n");
+    printf("Enter Day Number \n");
     scanf("%d", &day);
     if (day >= 1 && day <= 7)
     {
@@ -17,6 +18,5 @@ int main()
     {
         printf("%s", days[0]);
     }
-
     return 0;
 }
