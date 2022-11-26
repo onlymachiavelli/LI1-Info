b561b12177b9daacdd1b34f3f09c4462fbdc64b9
onlymachiavelli <tmakaveli643@gmail.com>
2022-11-26 01:31:39 +0100

    photoshop files

--- TP/TP6/ex3.c
+++ TP/TP6/ex3.c
@@ -1,9 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
 
-    char line[];
+    char line[200];
+    char list[10][200];
+    char word[] = "";
+    printf("Enter the words ! ");
+    gets(line);
+    int j = 0;
 
+    for (int i = 0; i < strlen(line); i++)
+    {
+        if (line[i] == ' ')
+        {
+            list[j] = word;
+        }
+        else
+        {
+
+            word += line[i];
+        }
+    }
     return 0;
 }
