451742a3560b9d5f760b6cde77111f20eed44e9c
onlymachiavelli <tmakaveli643@gmail.com>
2022-11-27 22:37:06 +0100

    ex7

--- TP/TP6/ex7.c
+++ TP/TP6/ex7.c
@@ -10,5 +10,25 @@ int main()
         printf("Enter the line ! \n");
         gets(line);
     } while (strlen(line) == 0);
+
+    char voy[12] = "aeiouyAEIOUY";
+    int check;
+    char newString[100] = "";
+    for (int i = 0; i < strlen(line); i++)
+    {
+        check = 0;
+        for (int j = 0; j < 12; j++)
+        {
+            if (line[i] == voy[j])
+            {
+                check = 1;
+            }
+        }
+        if (!check)
+        {
+            newString += line[i];
+        }
+    }
+
     return 0;
 }
