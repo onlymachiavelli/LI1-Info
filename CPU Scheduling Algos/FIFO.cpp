3538993092fe5920084bc8d86fd5971837c11eda
onlymachiavelli <tmakaveli643@gmail.com>
2023-01-03 01:57:31 +0100

    Fifo Algorythm

--- CPU Scheduling Algos/FIFO.cpp
+++ CPU Scheduling Algos/FIFO.cpp
@@ -1,81 +1,44 @@
-#include  <bits/stdc++.h>
+#include <bits/stdc++.h>
 
-using namespace std ; 
-typedef struct CPU{
-    string name ; 
-    int duration ; 
-    int arrival ; 
-}cpu;
+using namespace std;
 
 
-int getData() {
-    int n; 
-    cout << "Enter the data ! " << endl ; 
-    cin >> n ; 
-
-    if (n > 0 ) return n ; 
-    return getData() ; 
-}
-
-string notEmpty () {
-    cout << "Enter the datas ! " << endl; 
-    string data ; 
-    cin >> data ; 
-    if (data.length() > 0) {
-        return data ; 
-    }
-    return notEmpty() ; 
-}
-
-void ZAWALI_SORT(int n , vector <CPU> *PROCESSORS){
-    bool check = false ; 
-    CPU swap ; 
-    while (!check) {
-        check = true ; 
-        for (int i = 0; i< n-1 ; i++) {
-            if (PROCESSORS[i].arrival >  PROCESSORS[i].arrival[i+1]) {
-                swap = PROCESSORS[i] ; 
-                PROCESSORS[i] = PROCESSORS[n-i-1] ; 
-                PROCESSORS[n-i-1] = swap ; 
-                check = false ; 
-            }
-        }
-    }
-}
 int main () {
-    
-
-    vector <CPU> PROCESSORS ; 
-    unsigned int n , CPU_NUMBER, START; 
-    cout << "How many datas are they ? " << endl; 
-    n = getData() ; 
-    cout << "How many CPU are they ? " << endl ; 
-    cout << "Enter your starting point !" << endl ; 
-    cin >> START; 
-    CPU_NUMBER = getData() ; 
-    CPU def ; 
-    unsigned int i = n ; 
-
-    while (i--) {
-        cout << "Enter the Name of the CPU ! " << endl ; 
-        def.name = notEmpty() ; 
-        cout << "Enter the duration of this CPU's data ! " << endl ; 
-        def.duration = getData() ; 
-        cout << "Enter the Arrival time of this CPU's data ! " << endl ; 
-        def.arrival = getData() ; 
-        PROCESSORS.push_back(def) ; 
+    //fifo algorythm 
+    int n;
+    cout << "Enter the number of processes: " ;
+    cin >> n;
+    int burst_time[n];
+    int arrival_time[n];
+    int waiting_time[n];
+    int turn_around_time[n];
+    int total_waiting_time = 0;
+    int total_turn_around_time = 0;
+    cout << "Enter the burst time of the processes: " ;
+    for ( int i = 0 ; i < n; i++) {
+        cin >> burst_time[i];
     }
-    //sorting the shit out of it 
-    ZAWALI_SORT(n, &PROCESSORS ) ; 
-    float Res = abs(PROCESSORS[0].duration - START) / n ; 
-    START = PROCESSORS[0] ; 
-    for (int i =1 ; i  < n ; i++) {
-        Res += abs(PROCESSORS[i].duration - START) / n ;
-        START = PROCESSORS[i] ; 
-    }   
-
-
-
+    cout << "Enter the arrival time of the processes: " ;
+    for ( int i = 0 ; i < n; i++) {
+        cin >> arrival_time[i];
+    }
+    waiting_time[ 0 ] = 0;
+    for ( int i = 1 ; i < n; i++) {
+        waiting_time[i] = burst_time[i - 1] + waiting_time[i - 1];
+    }
+    for ( int i = 0 ; i < n; i++) {
+        turn_around_time[i] = burst_time[i] + waiting_time[i];
+    }
+    for ( int i = 0 ; i < n; i++) {
+        total_waiting_time += waiting_time[i];
+        total_turn_around_time += turn_around_time[i];
+    }
+    cout << "Process\tBurst Time\tArrival Time\tWaiting Time\tTurn Around Time" << endl;
+    for ( int i = 0 ; i < n; i++) {
+        cout << i + 1 << "\t" << burst_time[i] << "\t\t" << arrival_time[i] << "\t\t" << waiting_time[i] << "\t\t" << turn_around_time[i] << endl;
+    }
+    cout << "Average Waiting Time: " << ( float ) total_waiting_time / n << endl;
+    cout << "Average Turn Around Time: " << ( float ) total_turn_around_time / n << endl;
     
-    return 0 ; 
+    return 0 ;
 }
