2b6a714e2e15029abfde0e1aa701401aeaa447fd
Alaa Barka <tmakaveli643@gmail.com>
2023-02-08 11:05:10 -0800

    preparing pdf file for algo

---
+++ SEM2/C/firstLesson.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main () {
+
+
+    int n ; 
+    printf("Enter N ! \n") ; 
+    scanf("%i", &n) ;
+    printf("\n");
+     for (int i = 0; i< n; i++) {
+        printf("%i \n", i) ; 
+
+    }
+
+
+    return 0; 
+}
