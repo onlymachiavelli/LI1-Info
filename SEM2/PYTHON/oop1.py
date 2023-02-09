28b76aaefb8bb402499e92a0bb1af1a5ce79457f
Alaa Barka <tmakaveli643@gmail.com>
2023-02-08 18:07:31 -0800

    exploring the classes in python

---
+++ SEM2/PYTHON/oop1.py
@@ -0,0 +1,19 @@
+class Person :
+    def __init__(self, firstName, lastName, sex, height, age):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.sex = sex 
+        self.height = height
+        self.age = age
+
+    def printAllDatas(self):
+        print(self.firstName)
+        print(self.lastName)
+        print(self.height)
+        print(self.age)
+        print(self.sex)
+        
+
+Alaa = Person("Alaa", "Barka", "male", 1.76, 20)
+
+print(Alaa.printAllDatas())
