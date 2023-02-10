9c122e3677185a9d0a01d99c360d8040f759faa8
Alaa Barka <tmakaveli643@gmail.com>
2023-02-10 11:02:03 +0100

    adding some things

--- SEM2/PYTHON/TP/ex10.py
+++ SEM2/PYTHON/TP/ex10.py
@@ -2,9 +2,10 @@ num = 0
 
 while num <= 0:
     num = int(input("Enter a positive integer \n"))
-
+counter = 0 
 #dividers 
 for i in range(1,(num+1)):
     if num % i == 0:
+        counter += 1 
         print(f"{i} is a divider of {num}")
-
+print(f"The sum of the dividers is {counter}")
--- SEM2/PYTHON/TP/ex9.py
+++ SEM2/PYTHON/TP/ex9.py
@@ -5,6 +5,7 @@ while n < 0:
     n = int(input("Enter a positive integer \n"))
 
 fact  = 1 
-for i in range(1,n+1):
-    fact *= i
+if n > 0:
+    for i in range(1,n+1):
+        fact *= i
 print(f"The factorial of {n} is {fact}")
