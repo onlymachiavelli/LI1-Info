584b20a02df995233485f3b559d265e3349a3daa
Alaa Barka <tmakaveli643@gmail.com>
2023-02-10 10:38:19 +0100

    ex 3 python

---
+++ SEM2/PYTHON/TP/ex2.py
@@ -0,0 +1,3 @@
+ray = float(input("Enter The Rayon \n"))
+print(f"The area is : {ray*ray*3.14}")
+print(f"The perimeter is : {2*ray*3.14}")
---
+++ SEM2/PYTHON/TP/ex3.py
@@ -0,0 +1,7 @@
+a = int(input("Enter a \n"))
+b = int(input("Enter b \n"))
+if a> b :
+    print(f"The max between {a} and {b} is {a}")
+else :
+    print(f"The max between {a} and {b} is {b}")
+10
