b27bf939fbb593fbc29b7b509fffe162e7dca005
Alaa Barka <tmakaveli643@gmail.com>
2023-02-11 20:11:20 +0100

    changes

--- SEM2/Atelier/TP2/ex3.c
+++ SEM2/Atelier/TP2/ex3.c
@@ -4,9 +4,19 @@
 int main () {
 
 
-    int arr[5] = {1,2,3,4,5} ; 
+    int n ; 
+    int arr[n] ; 
+    printf("Enter n \n");
+    scanf("%d", &n);
+    for (int i =0; i< n ;i++) {
+        scanf("%d", &arr[i]);
+    }
+
+
+
+
     int *p = arr , sum=0;
-    for (int i =0; i< 5 ;i++) {
+    for (int i =0; i< n ;i++) {
         sum += *p ; 
         p++ ;
     }
