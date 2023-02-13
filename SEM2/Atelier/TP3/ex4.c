93afeb4892e61fed055a912018775e476b793d70
Alaa Barka <tmakaveli643@gmail.com>
2023-02-13 22:43:02 +0100

    ex 4 tp3

---
+++ SEM2/Atelier/TP3/ex4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main () {
+
+
+
+    char *days[7] = {
+        "Monday" , "Tuesday" , "Wednesday" , "Thursday" , "Friday" , "Saturday", "Sunday"
+    } ; 
+
+
+    int day = 0 ; 
+    while (day < 1 || day > 7) {
+        printf("Enter the day number ! \n") ;
+        scanf("%d" , &day) ;
+    }
+
+    char **P = days ;
+    P += day - 1 ;
+    printf("The day is %s \n" , *P) ;
+
+    return 0; 
+}
