59a74e34a99b6b2180be9c084408f5bae8390485
Alaa Barka <tmakaveli643@gmail.com>
2023-02-13 22:29:59 +0100

    ex 2

--- SEM2/Atelier/TP3/ex2.c
+++ SEM2/Atelier/TP3/ex2.c
@@ -1,27 +1,34 @@
 #include <stdio.h> 
 
 
-
-
 int main () {
 
-    int n  ;
-    printf("Enter length \n") ; 
-    scanf("%d" , &n) ; 
-    int arr[n] ; 
-    for (int i =0 ; i< n ; i++) {
-        printf("Enter element of index %d\n" , i) ;
-        scanf("%d" , &arr[i]) ;  
-    }
-    int *p = arr +6  ; 
-    printf("\n%d\n" , *p) ; 
-    while (p < arr+3 ) {
-        printf("%d , ", *p  ) ; 
-    }
-    
-    for (int i =0 ; i< n; i++) {
-        printf("%d , ", arr[i]  ) ;     
+    int arr[10] ; 
+    //filling the array ! 
+    int *F = arr ; 
+    while (F < 10+arr  ) {
+        printf("Enter The data ! \n") ; 
+        scanf("%d" , F) ; 
+        ++F; 
     }
 
-   return 0 ; 
+    int *Pr1 = arr ; 
+    while (Pr1 < 10 + arr) {
+        printf("%d , " , *Pr1) ; 
+        ++Pr1 ;
+    }
+    printf("\n") ; 
+    int *Pr3 = arr + 7 ;
+    while (Pr3 < 10 + arr) {
+        *Pr3 += 1 ; 
+        ++Pr3 ;  
+    }
+
+    int *Prr = arr ; 
+        while (Prr < 10 + arr) {
+            printf("%d , " , *Prr) ; 
+            ++Prr ;
+        }
+
+    return 0 ; 
 }
