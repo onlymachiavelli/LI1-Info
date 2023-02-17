4810b3f57079cbaeb4a24a873400009874135ca9
Alaa Barka <tmakaveli643@gmail.com>
2023-02-17 10:39:45 +0100

    ex 2

---
+++ SEM2/PYTHON/TP3/ex2.py
@@ -0,0 +1,18 @@
+
+
+
+moy = 0 
+count = 0 
+while (True) :
+    
+    note = float(input("Enter note "))
+
+    
+    if (note>=0) :
+        moy = moy + note
+        count = count + 1
+    else :
+        break
+moy /= count 
+print(f"Moyenne of {count} subjects = {moy}")
+
