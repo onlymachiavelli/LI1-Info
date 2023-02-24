e142eecb40c1822b667c40df47caa2ec93f0c831
Alaa Barka <tmakaveli643@gmail.com>
2023-02-24 13:23:38 +0100

    ex 5

--- SEM2/Atelier/TP3/ex5.c
+++ SEM2/Atelier/TP3/ex5.c
@@ -1,5 +1,35 @@
 #include <stdio.h>
 
+#include <string.h>
+ int isPalindrome(char *str)
+{
+    int len = strlen(str) - 1;
+    char *start = str;
+    char *end = str + len ;
+    while (end > start)
+    {
+        if (*start != *end)
+            return 0;
+        start++;
+        end--;
+
+    }
+    return 1;
+}
 int main () {
     
+
+    char *string ; 
+    //get the string 
+    printf("Enter a string : ");
+    scanf("%s",string);
+    
+    if(isPalindrome(string))
+        printf("The string is a palindrome");
+    else
+        printf("The string is not a palindrome");
+    //check palindrome using pointer 
+    
+    return 0;
+
 }
