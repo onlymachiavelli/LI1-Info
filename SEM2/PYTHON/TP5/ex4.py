60760a3f22700ade7774566569b0ec05cac3d081
Alaa Barka <tmakaveli643@gmail.com>
2023-03-10 11:40:52 +0100

    ex 4

---
+++ SEM2/PYTHON/TP5/ex4.py
@@ -0,0 +1,32 @@
+
+
+def sumDiv (n:int) -> int :
+    res = 0
+    for i in range (1, n) :
+        if ( n%2 ==0):sum += i
+
+    return res 
+
+def prime(num) :
+
+    check = True 
+    for i in range (2,  (num//2)+1) :
+        if (num % i == 0) :
+            print(i)
+            check = False 
+            break
+
+    return check 
+
+
+
+def perfect (n:int) -> bool :
+    return sumDiv(n) == n
+
+def luckyNumber (n:int ) ->int :
+    
+    for i in range (1, n) :
+        if (not prime(n+i+i**2)) :
+            return False
+        
+    return True 
