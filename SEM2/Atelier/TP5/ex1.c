6b9132da80622dfa8a5d5952b438194bd31a9143
Alaa Barka <tmakaveli643@gmail.com>
2023-03-10 12:31:18 +0100

    ex 1 tp5

---
+++ SEM2/Atelier/TP5/ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+typedef struct {
+    int head ;
+    NODE*next;
+}NODE;
+int main () {
+    NODE *HEAD = (NODE*)malloc(sizeof(NODE));
+    NODE*PREV = NULL;
+    
+    return 0; 
+}
