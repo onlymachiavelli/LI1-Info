59269e59e2ce58690813410a373083485dda6fe8
Alaa Barka <tmakaveli643@gmail.com>
2023-04-13 15:20:25 +0100

    ex1

--- SEM2/Atelier/TPFUNF/ex1.c
+++ SEM2/Atelier/TPFUNF/ex1.c
@@ -1,51 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node {
-    int data;
-    struct node *next;
-} NODE;
+typedef struct  {
+    int val; 
+    struct Node *next; 
+}Node;
 
-NODE *fill(int n) {
-    NODE *head = (NODE*)malloc(sizeof(NODE));
-    NODE *prev_node = NULL;
-    int data;
+int main () {
+    //reserve one linked list 
+    Node *head = (Node*)malloc(sizeof(Node)) ; 
 
-    for (int i = 0; i < n; i++) {
-        printf("Enter data: ");
-        scanf("%d", &data);
-        NODE *new_node = (NODE *)malloc(sizeof(NODE));
-        new_node->data = data;
-        new_node->next = NULL;
+    printf("Enter any data ! \n");
+    scanf("%d", &head->val);
 
-        if (prev_node != NULL) {
-            prev_node->next = new_node;
-        }
-        else {
-            head = new_node;
-        }
-
-        prev_node = new_node;
-    }
-
-    return head;
-}
-
-void print(NODE *node) {
-    while (node != NULL) {
-        printf("%d\n", node->data);
-        node = node->next;
-    }
-}
-
-int main() {
-
-    int n ; 
-    printf("Enter N \n") ;
-    scanf("%d", &n);
-
-    NODE *node = fill(n);
-    print(node);
-
-    return 0;
+    printf("The first data is : %d" , head->val); 
 }
