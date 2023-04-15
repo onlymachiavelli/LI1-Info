8f6ae8d5d584cb824bd1f73fe54e783c6fbb507e
Alaa Barka <tmakaveli643@gmail.com>
2023-04-15 02:55:28 +0100

    ex 3

--- SEM2/PYTHON/TP6/ex3.py
+++ SEM2/PYTHON/TP6/ex3.py
@@ -1,6 +1,7 @@
+myString = input("Enter a string to calculate the a character  : ")
 
 
-myString = input("Enter a string : ")
+for i in range (len(myString)) :
+    if myString[i] == 'a' :
+        print(f"the character 'a' is at the position {i}")
 
-fWord = myString[:myString.find(" ")]
-print(fWord)
