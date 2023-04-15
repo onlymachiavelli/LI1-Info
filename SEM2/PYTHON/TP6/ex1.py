ef23d9a0af219af0bb939583a84635923cad2e1a
Alaa Barka <tmakaveli643@gmail.com>
2023-04-15 02:49:50 +0100

    ex1

---
+++ SEM2/PYTHON/TP6/ex1.py
@@ -0,0 +1,14 @@
+myString = input("Enter the string : ") 
+
+voy = 0
+
+voys = "aeouyi"
+
+
+for char in myString : 
+    if char in voy : 
+        voy += 1
+
+print(f"For the string {myString} there are {voy}")
+
+
