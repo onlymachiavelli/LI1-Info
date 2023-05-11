08b9f4fd3e2515696c02ba67cd0d47a97e4cd40c
Alaa Barka <tmakaveli643@gmail.com>
2023-05-11 14:51:34 +0100

    fixing the tp exam

--- Nukes/main.c
+++ Nukes/main.c
@@ -1,114 +1,88 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct Node {
+    int head;
+    struct Node* next;
+} Node;
 
+Node* fill(int n) {
+    Node* head = NULL;
+    Node* tmp = NULL;
 
-typedef struct Node{
-    int head; 
-    struct Node *next; 
-}Node;
-
-
-Node *fill(int n) {
-    Node *head =NULL ;
-
-    Node *tmp = NULL ;
-
-    for (int i =0 ;i < n; i++) {
-        Node* new = (Node * )malloc(sizeof(Node)) ;
-        printf("ENTER DATA ! \n") ;
-        scanf("%d" , &new->head) ; 
+    for (int i = 0; i < n; i++) {
+        Node* new_node = (Node*) malloc(sizeof(Node));
+        printf("ENTER DATA ! \n");
+        scanf("%d", &new_node->head);
+        new_node->next = NULL;
+        
         if (head == NULL) {
-            head = new ; 
-            head->next = NULL ;
-            tmp = head; 
-        } 
-        else {
-            tmp -> next = new; 
-            tmp = tmp->next ; 
+            head = new_node;
+            tmp = head;
+        } else {
+            tmp->next = new_node;
+            tmp = tmp->next;
         }
-
     }
 
-    return head; 
-
+    return head;
 }
 
+void show(Node* list) {
+    Node* tmp = list;
 
-
-
-void Show(Node *list, int n) {
-    
-
-    Node *tmp = list;
-    /*
-     while (tmp) {
-        printf("%d\n", tmp->head);
+    while (tmp != NULL) {
+        printf("%d ", tmp->head);
         tmp = tmp->next;
     }
-    */
-
-    for (int i =0;i<n;i++) {
-        printf("%d", tmp->head) ; 
-        tmp = tmp->next ; 
-    }
+    printf("\n");
 }
 
+void sort(Node** head) {
+    int swapped = 1;
+    while (swapped) {
+        swapped = 0;
+        Node* current = *head;
+        Node* prev = NULL;
 
-void Sort(Node ** head) {
-    Node *tmp = *head;
-        int check = 1 ; 
-        while (check) {
-            check = 0; 
-            while (tmp->next) {
-                if (tmp->head > tmp->next->head) {
-                    Node *thing = tmp->next; 
-                    tmp->next = tmp->next->next;
-                    thing->next = tmp;
-                    tmp = thing;
-                    check = 1;
+        while (current != NULL && current->next != NULL) {
+            if (current->head > current->next->head) {
+                Node* next = current->next;
+                current->next = next->next;
+                next->next = current;
+
+                if (prev == NULL) {
+                    *head = next;
+                } else {
+                    prev->next = next;
                 }
-                tmp=tmp->next ;
+
+                prev = next;
+                swapped = 1;
+            } else {
+                prev = current;
+                current = current->next;
             }
         }
-
-}
-
-void sort(Node **head, int n){
-    Node *tmp = *head;
-    int check = 1;
-    while (check) {
-        check = 0 ;
-        for (int i=0;i<n-1;i++){
-                if (tmp->head > tmp->next->head) {
-                    Node *data = (Node*) malloc(sizeof(Node)) ; 
-                    data->next = NULL ; 
-                    data->head = tmp->next->head; 
-                    data->next = tmp->next; 
-                    tmp = data; 
-                    check = 1;
-                }
-                tmp=tmp->next ;
-            }
     }
 }
 
-
-int main () {
-
-    int n; 
+int main() {
+    int n;
     while (1) {
-        printf("Enter n > 0 \n") ; 
-        scanf("%d" , &n);
-        if (n>0) break ; 
+        printf("Enter n > 0\n");
+        scanf("%d", &n);
+        if (n > 0) break;
     }
 
-    Node *head = fill(n) ; 
-    printf("Before Sorting ! \n"); 
+    Node* head = fill(n);
+    printf("Before Sorting:\n");
+    show(head);
 
-    Show(head, n);
-    printf("\nAfter Sorting \n") ; 
-    sort(&head, n) ; 
-    Show(head, n);
-    return 0 ; 
+    sort(&head);
+
+    printf("After Sorting:\n");
+    show(head);
+
+    return 0;
 }
